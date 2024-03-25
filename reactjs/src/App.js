bdfcfbd4773c98220be60a5d0983fcb9a69838ed
shehryar-munir <shehryar.munir@dubizzlelabs.com>
2024-03-25 12:29:16 +0500

    Implemented React Basics

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ProductCard from "./Components/ProductCard/ProductCard";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   //const products = null
+    const products = [
+        {
+            productColor: "Black",
+            productName: "Fossil",
+            productDescription: "Fossil Men’s Chronograph Quartz Silver Stainless Steel Black Dial 44mm Watch FS5236",
+            productPrice: "$100",
+            productImg: "https://royalwrist.pk/wp-content/uploads/2022/03/1-331-600x600.jpg",
+        },
+
+        {
+            productColor: "#D36B00",
+            productName: "Guess",
+            productDescription: "Guess Men’s Quartz Gold Stainless Steel Black Dial 44mm Watch W1315G2",
+            productPrice: "$100",
+            productImg: "https://royalwrist.pk/wp-content/uploads/2022/12/W1315G2-600x600.jpg",
+        },
+
+
+        {
+            productColor: "silver",
+            productName: "Citizen",
+            productDescription: "Citizen Armani Men’s Quartz Stainless Steel Beige Dial 43mm Watch AR11352",
+            productPrice: "$100",
+            productImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4oyWsdJynRpQvKEzGDrmkSPpLA-7hf0ddWQ&usqp=CAU",
+        },
+    ]
+
+    return (
+
+      <div style={{   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',display:"flex-wrap", flexDirection:"column"}}>
+          <div style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100vh', // Adjust the height as needed
+          }}>
+              {products && products ?  (
+                  products.map((prod, index) => {
+                      return <ProductCard productColor={prod.productColor} productName={prod.productName} productDescription={prod.productDescription} productPrice={prod.productPrice} productImg={prod.productImg} key={index} />
+                  })
+              ) : <h1> No Products Found</h1>}
+
+
+
+          </div>
+      </div>
   );
 }
 
---
+++ reactjs/src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Rating from '@mui/material/Rating'
+import  Button from '@mui/material/Button'
+import {Typography} from "@mui/material";
+export default function ProductCard({productImg, productName, productDescription, productPrice,productColor}) {
+    const [value, setValue] = React.useState(4);
+
+    const handleClick = (event) => {
+        event.preventDefault();
+         // alert all the information
+    }
+
+    return(
+
+        <div style={{background:"white" ,color:"black", margin:"5%"}}>
+
+            <div >
+
+
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <img
+                        height={"20%"}
+                        width={"50%"}
+                        src={productImg}
+                        alt={"Image"}/>
+                    <h1>
+                        {productName}
+                    </h1>
+
+                    <Typography style={{margin:"5%"}} variant="body2" color="text.secondary">
+                        {productDescription}
+                    </Typography>
+
+                    <h2 style={{color: productColor}}>
+                        {productPrice}
+                    </h2>
+
+                    <Rating
+                        name="simple-controlled"
+                        value={value}
+                        style={{padding: "2%"}}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                    />
+                    <div>
+                        <Button variant="contained" onClick={(e)=>        alert(`Product Name: ${productName} \nProduct Description: ${productDescription} \nProduct Price: ${productPrice}`)} style={{width: "500px", color: "white", background: productColor}}>Add
+                            to Cart</Button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
